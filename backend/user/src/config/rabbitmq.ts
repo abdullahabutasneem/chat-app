2651eb2947e12952a4837ea5abb73cb28b97240f
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2026-05-15 16:36:07 +0600

    Implement publish to queue chunk

--- backend/user/src/config/rabbitmq.ts
+++ backend/user/src/config/rabbitmq.ts
@@ -18,3 +18,16 @@ export const connectRabbitMQ = async () => {
         console.log("Failed to connect to RabbitMQ", error);
     }
 }
+
+export const publishToQueue = async (queueName: string, message: any) => {
+    if (!channel) {
+        console.log("RabbitMQ channel not found");
+        return;
+    }
+
+    await channel.assertQueue(queueName, { durable: true });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(message)), {
+        persistent: true,
+    });
+    console.log(`📤 Message published to ${queueName}`);
+}
