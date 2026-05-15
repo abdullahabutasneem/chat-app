63581a3e65e482baaf479a1d09f31e446c570604
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2026-05-15 15:03:56 +0600

    Establish connection to Redis

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,9 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { createClient } from "redis";
 
 dotenv.config();
 connectDB();
+
+export const redisClient = createClient({
+    url: process.env.REDIS_URL || "",
+});
+
+redisClient.connect().then(() => {
+    console.log("Connected to Redis");
+}).catch(err => {
+    console.log("Failed to connect to Redis", err);
+})
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
