a5ce6ab7a74de3f3ddd31ce3641eb16b67206441
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2026-05-15 11:55:32 +0600

    Connected to MongoDB

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
+connectDB();
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
