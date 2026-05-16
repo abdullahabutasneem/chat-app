4c896977f7c8936d6b30420fcfed98431cbe6661
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2026-05-16 18:14:37 +0600

    Initiate mail server implementation

--- backend/mail/dist/index.js
+++ backend/mail/dist/index.js
@@ -1,10 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
-
 const app = express();
-
 app.listen(process.env.PORT, () => {
     console.log(`Mail Server is running on port ${process.env.PORT}`);
 });
