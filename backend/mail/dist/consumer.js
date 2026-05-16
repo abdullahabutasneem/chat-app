ea1307b80f0007d4be3b460c2e8dc62af22e0855
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2026-05-16 18:32:02 +0600

    OTP consumer file implementation

---
+++ backend/mail/dist/consumer.js
@@ -0,0 +1,48 @@
+import amqp from "amqplib";
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+export const startSendOtpConsumer = async () => {
+    try {
+        const connection = await amqp.connect({
+            protocol: "amqp",
+            hostname: process.env.RABBITMQ_HOST || "",
+            port: 5672,
+            username: process.env.RABBITMQ_USERNAME || "",
+            password: process.env.RABBITMQ_PASSWORD || "",
+        });
+        const channel = await connection.createChannel();
+        const queueName = "send-otp";
+        await channel.assertQueue(queueName, { durable: true });
+        console.log("✅ Mail service consumer started. listening to queue: ", queueName);
+        channel.consume(queueName, async (msg) => {
+            if (msg) {
+                try {
+                    const { to, subject, body } = JSON.parse(msg.content.toString());
+                    const transporter = nodemailer.createTransport({
+                        host: "smtp.gmail.com",
+                        port: 465,
+                        auth: {
+                            user: process.env.USER || "",
+                            pass: process.env.PASSWORD || "",
+                        },
+                    });
+                    await transporter.sendMail({
+                        from: "Chat app",
+                        to,
+                        subject,
+                        text: body,
+                    });
+                    console.log(`OTP mail sent to ${to} successfully`);
+                    channel.ack(msg);
+                }
+                catch (error) {
+                    console.log("Failed to send OTP mail", error);
+                }
+            }
+        });
+    }
+    catch (error) {
+        console.log("Failed to start RabbitMQ consumer", error);
+    }
+};
