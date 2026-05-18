5f13cef0c312a12655737f912e69e380e010d997
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2026-05-18 14:42:18 +0600

    Connected MongoDB for chat service

---
+++ backend/chat/dist/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+        throw new Error("MONGO_URI is not defined in the environment variables");
+    }
+    try {
+        await mongoose.connect(mongoURI, {
+            dbName: "microservice-chatapp-db",
+        });
+        console.log("Connected to MongoDB");
+    }
+    catch (error) {
+        console.error("Failed to connect to MongoDB", error);
+        process.exit(1);
+    }
+};
+export default connectDB;
